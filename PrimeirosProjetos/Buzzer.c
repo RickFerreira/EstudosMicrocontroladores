3591e94d9549bdb64ff3c41ef42397f374cac086
Richard Ferreira <richard.salviano@academico.ifpb.edu.br>
2022-10-14 10:59:04 -0300

    Update Buzzer.c

--- PrimeirosProjetos/Buzzer.c
+++ PrimeirosProjetos/Buzzer.c
@@ -12,8 +12,11 @@
 #define LED_OFF6 GPIOA->ODR |= (1<<6) //Desliga o led
 #define LED_ON6 GPIOA->ODR &= ~(1<<6) //Liga o led
 
-#define BUZZER_OFF7 GPIOA->ODR |= (1<<7)   //Desliga o led
-#define BUZZER_ON7 GPIOA->ODR &= ~(1<<7)   //Liga o led
+#define LED_OFF7 GPIOA->ODR |= (1<<7)   //Desliga o led
+#define LED_ON7 GPIOA->ODR &= ~(1<<7)   //Liga o led
+
+#define BUZZER_OFF8 GPIOA->ODR |= (1<<8)   //Desliga o buzzer
+#define BUZZER_ON8 GPIOA->ODR &= ~(1<<8)   //Liga o buzzer
 
 int main(void)
 {
@@ -23,12 +26,17 @@ int main(void)
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; //habilita o clock do GPIOA
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN; //habilita o clock do GPIOE
 
+    //Leds
     GPIOA->MODER |= (0b01 << 12);    //configura o pino PA6 como saída (LED D2)
-    GPIOA->MODER |= (0b01 << 14);    //configura o pino PA7 como saída (BUZZER D3)
+    GPIOA->MODER |= (0b01 << 14);    //configura o pino PA7 como saída (LED D3)
+
+    //Buzzer
+    GPIOA->MODER |= (0b01 << 16);    //configura o pino PA8 como saída (BUZZER)
 
     //GPIOE->MODER &= ~(0b11 << 6);    //configurando o pino PE3 como entrada (botão k1)
     //GPIOE->PUPDR |= (0b01 << 6);     //liga o resistor de pull-up no pino PE3 (pra garantir nível alto quando a chave estiver solta)
 
+    //Botões
     GPIOE->MODER &= ~(0b11 << 8);    //configurando o pino PE4 como entrada (botão k0)
     GPIOE->PUPDR |= (0b01 << 8);     //liga o resistor de pull-up no pino PE4 (pra garantir nível alto quando a chave estiver solta)
 
@@ -40,14 +48,17 @@ int main(void)
     	for(int i=0; i<=3; i++)
     	{
         	LED_ON6;
-        	BUZZER_ON7;
+        	LED_ON7;
+        	BUZZER_ON8;
         	Delay_ms(50); //Chama a função passando como parametros 100 milissegundos
         	LED_OFF6;
-        	BUZZER_OFF7;
+        	LED_OFF7;
+        	BUZZER_OFF8;
         	Delay_ms(50); //Chama a função passando como parametros 100 milissegundos
     	}
     	LED_OFF6;
-    	BUZZER_OFF7;
+    	LED_OFF7;
+    	BUZZER_OFF8;
     	Delay_ms(1200); //Chama a função passando como parametros 100 milissegundos
     }
 
